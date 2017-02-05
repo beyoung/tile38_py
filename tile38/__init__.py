cdd7cea7c2f6ee4b083d86720c7845b8726e2f84
beyoung <tuwenyoung@gmail.com>
2017-02-05 09:02:35 +0800

    tile 38

---
+++ tile38/__init__.py
@@ -0,0 +1,9 @@
+# !/user/bin/env/python
+# -*- coding: utf-8 -*-
+# version: v0.0.1
+# author: youth
+# contact: tuwenyoung@gmail.com
+# project: tile38_py
+# filename: __init__.py.py
+# datetime: 2017-01-13 22:20
+# description: 
---
+++ tile38/client.py
@@ -0,0 +1,9 @@
+# !/user/bin/env/python
+# -*- coding: utf-8 -*-
+# version: v0.0.1
+# author: youth
+# contact: tuwenyoung@gmail.com
+# project: tile38_py
+# filename: client.py
+# datetime: 2017-01-13 22:21
+# description:
